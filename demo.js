496669c037f24cdb9da7854a5830693566078026
HarshaMacherla <macherlaharsha997@gmail.com>
2023-02-09 16:43:00 +0530

    Changed font color of the list items & added background color

--- demo.js
+++ demo.js
@@ -1,5 +1,11 @@
-var newItem = document.getElementsByClassName('last-group-item');
-newItem[0].style.backgroundColor = 'green';
+var items = document.getElementsByClassName('list-group-item');
+items[1].style.backgroundColor = 'green';
+items[2].style.display = 'none';
 
-var lastItem = document.getElementsByTagName('li');
-lastItem[lastItem.length - 1].style.fontWeight = 'bold';
+
+var listItems = document.querySelectorAll('.list-group-item');
+listItems[1].style.color = 'green';
+var odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = 'green';
+}
