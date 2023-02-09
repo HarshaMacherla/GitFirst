8227d06539bfe75dd38d5f76930c8bd6f41b1e4b
HarshaMacherla <macherlaharsha997@gmail.com>
2023-02-09 15:22:20 +0530

    New Item has been added to the list

--- demo.js
+++ demo.js
@@ -1,6 +1,5 @@
-var listItems = document.getElementsByClassName("list-group-item");
-listItems[2].style.backgroundColor = 'green';
+var newItem = document.getElementsByClassName('last-group-item');
+newItem[0].style.backgroundColor = 'green';
 
-for (let i = 0; i < listItems.length; i++) {
-    listItems[i].style.fontWeight = 'bold';
-}
+var lastItem = document.getElementsByTagName('li');
+lastItem[lastItem.length - 1].style.fontWeight = 'bold';
