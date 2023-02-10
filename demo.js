e826f7445397dddbb11a19cce9946b613d80b2fa
HarshaMacherla <macherlaharsha997@gmail.com>
2023-02-10 09:11:46 +0530

    Added Functionality: 1. Can add new items to the list 2. Filter functionality

--- demo.js
+++ demo.js
@@ -1,5 +1,6 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
@@ -7,19 +8,25 @@ form.addEventListener('submit', addItem);
 // Delete event
 itemList.addEventListener('click', removeItem);
 
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
 // Add item
 function addItem(e) {
     e.preventDefault();
     
     // Get input value
     var newItem = document.getElementById('item').value;
+    var newDescription = document.getElementById('description').value;
 
     // Create new li element
     var li = document.createElement('li');
     li.className = 'list-group-item';
     
+
     // Add text node with input value
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(" " + newDescription));
 
     // Create del buttom element
     var deleteBtn = document.createElement('button');
@@ -52,4 +59,23 @@ function removeItem(e) {
             itemList.removeChild(li);
         }
     }
+}
+
+// Filter Items
+function filterItems(e) {
+    // Convert text to lowercase
+    var text = e.target.value.toLowerCase();
+
+    // Storing the list items into a collection
+    var items = document.getElementsByTagName('li');
+
+    // Converting to an array
+    Array.from(items).forEach(function(item) {
+        var itemName = item.firstChild.textContent;
+        var itemDescription = item.childNodes[1].textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1 || itemDescription.toLowerCase().indexOf(text) != -1)
+            item.style.display = 'block';
+        else
+            item.style.display = 'none';
+    });
 }
